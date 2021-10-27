bd6fb853527d7dbd350f2a75be772cde32b04e8e
cemtaskin <cemtaskin82@gmail.com>
2021-10-27 15:36:14 +0300

    project developed by using state

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,6 +2,8 @@ import React,{useState} from 'react';
 import {View,Text,StyleSheet} from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
+const COLOR_INCREMENT = 15;
+
 const SquareScreen = () => {
 
     
@@ -9,26 +11,43 @@ const SquareScreen = () => {
     const [green,setGreen] = useState(0);
     const [blue,setBlue] = useState(0);
     
-    
+
     console.log(red);
     //console.log(green);
     //console.log(blue);
     
+    const setColor = (color,change)=>{
+        // color === 'red' , 'green' , 'blue'
+        // change === +15 , -15
+        switch(color){
+          case 'red':
+            red+change>255 || red+ change<0 ? null:setRed(red+change);
+            return;  
+          case 'green':
+            green+change>255 || green+ change<0 ? null:setGreen(green+change);              
+            return;
+          case 'blue':
+            blue+change>255 || blue+ change<0 ? null:setBlue(blue+change);
+            return;
+          default:
+              return;
+        }
+    };
 
     return <View>
         <Text>Square Screen</Text>
         <ColorCounter 
             color="Red"
-            onIncrease={()=>{setRed(red+1)}}
-            onDecrease={()=>{setRed(red-1)}}
+            onIncrease={()=>setColor('red',COLOR_INCREMENT)}
+            onDecrease={()=>setColor('red',-1*COLOR_INCREMENT)}
             />
         <ColorCounter color="Green"
-            onIncrease={()=>{setGreen(green+1)}}
-            onDecrease={()=>{setGreen(green-1)}}           
+            onIncrease={()=>{setColor('green',COLOR_INCREMENT)}}
+            onDecrease={()=>{setColor('green',-1*COLOR_INCREMENT)}}           
         />
         <ColorCounter color="Blue"
-        onIncrease={()=>{setBlue(blue+1)}}
-        onDecrease={()=>{setBlue(blue-1)}}
+        onIncrease={()=>{setColor('blue',COLOR_INCREMENT)}}
+        onDecrease={()=>{setColor('blue'-1*COLOR_INCREMENT)}}
       />
 
       <View
